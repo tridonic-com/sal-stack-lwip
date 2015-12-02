e4e88b7efe59e8e1e05887b308bb7fd151096fbc
Ivan Adzic <ivan.adzic@tridonic.com>
2015-12-02 09:44:57 +0100

    added define YOTTA_CFG_MBED_OS_NET_STACKS_DRIVER to support our target

--- source/lwip/core/memp.c
+++ source/lwip/core/memp.c
@@ -170,6 +170,8 @@ static u8_t *const memp_bases[] = {
 #  define ETHMEM_SECTION __attribute((section("AHBSRAM1")))
 #elif defined(TARGET_LPC4088) || defined(TARGET_K64F) || defined(TARGET_RZ_A1H)
 #  define ETHMEM_SECTION 
+#elif defined(YOTTA_CFG_MBED_OS_NET_STACKS_DRIVER)
+#  define ETHMEM_SECTION 
 #endif
 
 /** This is the actual memory used by the pools (all pools in one big block). */
